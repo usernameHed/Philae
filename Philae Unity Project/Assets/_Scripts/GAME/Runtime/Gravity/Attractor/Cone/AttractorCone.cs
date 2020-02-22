4554f426b3e838d31986d8d2a61329919da78fe1
usernameHed <ugo@belfiore.fr>
2020-02-22 12:03:52 +0100

    Prepare Cone

---
+++ Philae Unity Project/Assets/_Scripts/GAME/Runtime/Gravity/Attractor/Cone/AttractorCone.cs
@@ -0,0 +1,56 @@
+﻿using hedCommon.geometry.shape3d;
+using philae.gravity.attractor.logic;
+using philae.gravity.graviton;
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace philae.gravity.attractor
+{
+    public class AttractorCone : Attractor
+    {
+        //[SerializeField, OnValueChanged("ChangeConeSettings", true)]
+        //protected ExtCylinder _cylinder = default;
+
+        public override void InitOnCreation(List<AttractorListerLogic> attractorListerLogic)
+        {
+            base.InitOnCreation(attractorListerLogic);
+            //_cylinder = new ExtCylinder(Position, Rotation, LocalScale, 0.5f, 4f);
+        }
+
+        public override Vector3 GetClosestPoint(Graviton graviton, out bool canApplyGravity)
+        {
+            /*
+            Vector3 closestPoint = _cylinder.GetClosestPoint(graviton.Position);
+            Vector3 position = this.GetRightPosWithRange(graviton.Position, closestPoint, _minRangeWithScale / 2, _maxRangeWithScale / 2, out bool outOfRange);
+            canApplyGravity = !outOfRange;
+            AddOrRemoveGravitonFromList(graviton, canApplyGravity);
+            return (position);
+            */
+            canApplyGravity = false;
+            return (Vector3.zero);
+        }
+
+        /*
+        public void ChangeConeSettings()
+        {
+            _cylinder.MoveSphape(Position, Rotation, LocalScale, _cylinder.Radius, _cylinder.Lenght);
+        }
+        */
+
+        public override void Move()
+        {
+            //_cylinder.MoveSphape(Position, Rotation, LocalScale);
+        }
+
+#if UNITY_EDITOR
+        protected override void DrawRange(Color color)
+        {
+            //_cylinder.Draw(color);
+            //_cylinder.DrawWithExtraSize(Color.gray, new Vector3(_minRangeWithScale, _minRangeWithScale / 2, _minRangeWithScale));
+            //_cylinder.DrawWithExtraSize(color, new Vector3(_maxRangeWithScale, _maxRangeWithScale / 2, _maxRangeWithScale));
+        }
+#endif
+    }
+}
