c8f38f9cd92ad7b724caa33af59c56df0f8c8210
usernameHed <ugo@belfiore.fr>
2020-03-23 20:53:23 +0100

    Add all attractors override

---
+++ Philae Unity Project/Assets/_Scripts/GAME/Runtime/Gravity/Attractor/Quad/AttractorQuadGravityOverride.cs
@@ -0,0 +1,35 @@
+﻿using hedCommon.extension.runtime;
+using hedCommon.geometry.movable;
+using hedCommon.geometry.shape3d;
+using philae.gravity.attractor.gravityOverride;
+using philae.gravity.attractor.logic;
+using philae.gravity.graviton;
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace philae.gravity.attractor
+{
+    public class AttractorQuadGravityOverride : AttractorQuad
+    {
+        public GravityOverrideQuad GravityOverride;
+
+        
+        public override bool GetClosestPointIfWeCan(Graviton graviton, out Vector3 closestPoint)
+        {
+            bool canApplyGravity = _movableQuad.Quad.GetClosestPointIfWeCan(graviton.Position, GravityOverride, out closestPoint);
+
+            if (canApplyGravity)
+            {
+                closestPoint = this.GetRightPosWithRange(graviton.Position, closestPoint, SettingsLocal.MinRange, SettingsLocal.MaxRange, out bool outOfRange);
+                if (outOfRange)
+                {
+                    canApplyGravity = false;
+                }
+            }
+            AddOrRemoveGravitonFromList(graviton, canApplyGravity);
+            return (canApplyGravity);
+        }
+    }
+}
---
+++ Philae Unity Project/Assets/_Scripts/GAME/Runtime/Gravity/Attractor/Triangle/AttractorTriangleGravityOverride.cs
@@ -0,0 +1,36 @@
+﻿using hedCommon.extension.runtime;
+using hedCommon.geometry.movable;
+using hedCommon.geometry.shape3d;
+using philae.gravity.attractor.gravityOverride;
+using philae.gravity.attractor.logic;
+using philae.gravity.graviton;
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace philae.gravity.attractor
+{
+    public class AttractorTriangleGravityOverride : AttractorTriangle
+    {
+        //public GravityOverrideLineTopDown GravityOverride;
+
+        /*
+        public override bool GetClosestPointIfWeCan(Graviton graviton, out Vector3 closestPoint)
+        {
+            bool canApplyGravity = _movableCapsule.Capsule.GetClosestPointIfWeCan(graviton.Position, GravityOverride, out closestPoint);
+
+            if (canApplyGravity)
+            {
+                closestPoint = this.GetRightPosWithRange(graviton.Position, closestPoint, SettingsLocal.MinRange, SettingsLocal.MaxRange, out bool outOfRange);
+                if (outOfRange)
+                {
+                    canApplyGravity = false;
+                }
+            }
+            AddOrRemoveGravitonFromList(graviton, canApplyGravity);
+            return (canApplyGravity);
+        }
+        */
+    }
+}
