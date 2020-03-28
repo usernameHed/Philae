b4d591320f23a27ad8d683143787083e7417b310
usernameHed <ugo@belfiore.fr>
2020-03-28 04:15:51 +0100

    Add Sphere Component Editor

---
+++ Philae Unity Project/Assets/_Scripts/Common Scripts/_HedToolsComponents/Editor/SphereCollider/FitSphere.cs
@@ -0,0 +1,108 @@
+﻿using hedCommon.extension.runtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using static UnityEditor.EditorGUILayout;
+
+namespace ExtUnityComponents.collider
+{
+    public class FitSphere
+    {
+        private SphereCollider[] _currentTargets = null;
+        private DecoratorComponentsEditor _currentEditor;
+        private MeshFilter[] _meshFilter;
+
+        public void Init(SphereCollider[] parent, DecoratorComponentsEditor current)
+        {
+            _currentTargets = parent;
+            _currentEditor = current;
+
+            SetupMeshFilter();
+        }
+
+        private void SetupMeshFilter()
+        {
+            _meshFilter = new MeshFilter[_currentTargets.Length];
+            for (int i = 0; i < _currentTargets.Length; i++)
+            {
+                _meshFilter[i] = _currentTargets[i].GetComponent<MeshFilter>();
+            }
+        }
+
+        public bool IsNullOrEmptyOrEmptyContent()
+        {
+            if (_meshFilter == null)
+            {
+                return (true);
+            }
+            if (_meshFilter.Length == 0)
+            {
+                return (true);
+            }
+            for (int i = 0; i < _meshFilter.Length; i++)
+            {
+                if (_meshFilter[i] == null)
+                {
+                    return (true);
+                }
+            }
+            return (false);
+        }
+
+        public void DisplayFitMesh()
+        {
+            using (HorizontalScope horizontalScope = new HorizontalScope(EditorStyles.helpBox))
+            {
+                GUILayout.Label("Fit collider to the mesh:");
+
+                if (!IsNullOrEmptyOrEmptyContent())
+                {
+                    if (GUILayout.Button("Fit"))
+                    {
+                        ApplyFitMeshs();
+                    }
+                }
+                else
+                {
+                    EditorGUI.BeginDisabledGroup(true);
+                    {
+                        GUILayout.Button("No MeshFilter");
+                    }
+                    EditorGUI.EndDisabledGroup();
+                }
+
+            }
+        }
+
+        private void ApplyFitMeshs()
+        {
+            for (int k = 0; k < _currentTargets.Length; k++)
+            {
+                List<Vector3> positionChilds = new List<Vector3>();
+
+                Undo.RecordObject(_currentTargets[k], "datas Sphere Collider");
+
+
+                MeshFilter parent = _meshFilter[k];
+                if (parent == null)
+                {
+                    continue;
+                }
+
+                Bounds bigBounds = parent.sharedMesh.bounds;
+
+                MeshFilter[] allChilds = _currentTargets[k].gameObject.GetExtComponentsInChildrens<MeshFilter>(99, false);
+                for (int i = 0; i < allChilds.Length; i++)
+                {
+                    //Debug.Log(allChilds[i].name);
+                    positionChilds.Add(allChilds[i].transform.position);
+                    bigBounds.Encapsulate(allChilds[i].sharedMesh.bounds);
+                }
+
+                _currentEditor.GetTargetIndex<SphereCollider>(k).radius = bigBounds.size.Maximum() / 2;
+                _currentEditor.GetTargetIndex<SphereCollider>(k).center = bigBounds.center;
+            }
+        }
+    }
+}
---
+++ Philae Unity Project/Assets/_Scripts/Common Scripts/_HedToolsComponents/Editor/SphereCollider/SphereColliderEditor.cs
@@ -0,0 +1,40 @@
+﻿using hedCommon.extension.runtime;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+using static UnityEditor.EditorGUILayout;
+
+
+/// <summary>
+///
+/// </summary>
+namespace ExtUnityComponents.collider
+{
+    [CustomEditor(typeof(SphereCollider))]
+    [CanEditMultipleObjects]
+    public class SphereColliderEditor : DecoratorComponentsEditor
+    {
+        private FitSphere _fitSphere = new FitSphere();
+
+        public SphereColliderEditor()
+            : base(BUILT_IN_EDITOR_COMPONENTS.SphereColliderEditor)
+        {
+
+        }
+
+        /// <summary>
+        /// init the targets
+        /// </summary>
+        protected override void InitOnFirstInspectorGUI()
+        {
+            _fitSphere.Init(GetTargets<SphereCollider>(), this);
+        }
+
+        protected override void OnCustomInspectorGUI()
+        {
+            _fitSphere.DisplayFitMesh();
+        }
+    }
+}
