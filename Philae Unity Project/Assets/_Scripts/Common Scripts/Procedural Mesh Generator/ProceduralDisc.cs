def83bc97d798d7cfb8a31dc5f7e8619fc6bec36
usernameHed <ugo@belfiore.fr>
2020-03-28 21:49:34 +0100

    Add Procedural Disc

---
+++ Philae Unity Project/Assets/_Scripts/Common Scripts/Procedural Mesh Generator/ProceduralDisc.cs
@@ -0,0 +1,125 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+using Sirenix.OdinInspector;
+using hedCommon.extension.runtime;
+
+namespace hedCommon.procedural
+{
+    /// <summary>
+    /// Plane Description
+    /// </summary>
+    public class ProceduralDisc : Generate
+    {
+        [SerializeField]
+        protected float _radius = 0.5f;
+        [SerializeField]
+        private int _sides = 18;
+
+        private int nbVerticesCap;
+        protected float _topRadius = 0f;
+        private const float PI_2 = Mathf.PI * 2f;
+
+        /// <summary>
+        /// here generate the mesh...
+        /// </summary>
+        protected override void GenerateMesh()
+        {
+            nbVerticesCap = _sides + 1;
+
+            Debug.Log("generate Disc...");
+            CalculateVerticle();
+            CalculateNormals();
+            CalculateUvs();
+            CalculateTriangle();
+        }
+
+        /// <summary>
+        /// calculate verticle
+        /// </summary>
+        protected override void CalculateVerticle()
+        {
+            // bottom + top + sides
+            _vertices = new Vector3[_sides + 1];
+            int vert = 0;
+
+            // Bottom cap
+            _vertices[vert++] = new Vector3(0f, 0f, 0f);
+            while (vert <= _sides)
+            {
+                float rad = (float)vert / _sides * PI_2;
+                _vertices[vert] = new Vector3(Mathf.Cos(rad) * _radius, 0f, Mathf.Sin(rad) * _radius);
+                vert++;
+            }
+        }
+
+        /// <summary>
+        /// after having verticle, calculate normals of each points
+        /// </summary>
+        protected override void CalculateNormals()
+        {
+            // bottom + top + sides
+            _normales = new Vector3[_vertices.Length];
+            int vert = 0;
+
+            // Bottom cap
+            while (vert <= _sides)
+            {
+                _normales[vert++] = Vector3.up;
+            }
+        }
+
+        /// <summary>
+        /// calculate UV of each points;
+        /// </summary>
+        protected override void CalculateUvs()
+        {
+            _uvs = new Vector2[_vertices.Length];
+
+            // Bottom cap
+            int u = 0;
+            _uvs[u++] = new Vector2(0.5f, 0.5f);
+            while (u <= _sides)
+            {
+                float rad = (float)u / _sides * PI_2;
+                _uvs[u] = new Vector2(Mathf.Cos(rad) * .5f + .5f, Mathf.Sin(rad) * .5f + .5f);
+                u++;
+            }
+        }
+
+        /// <summary>
+        /// then save triangls of objects;
+        /// </summary>
+        protected override void CalculateTriangle()
+        {
+            int nbTriangles = _sides;
+            _triangles = new int[nbTriangles * 3 + 3];
+
+            // Bottom cap
+            int tri = 0;
+            int i = 0;
+            while (tri < _sides - 1)
+            {
+                _triangles[i] = tri + 2;
+                _triangles[i + 1] = tri + 1;
+                _triangles[i + 2] = 0;
+                tri++;
+                i += 3;
+            }
+            _triangles[i] = 1;
+            _triangles[i + 1] = tri + 1;
+            _triangles[i + 2] = 0;
+        }
+
+        /// <summary>
+        /// fit the meshCollider to the procedural shape
+        /// </summary>
+        public override void GenerateCollider()
+        {
+            MeshCollider mesh = gameObject.GetOrAddComponent<MeshCollider>();
+            MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
+            ExtColliders.AutoSizeCollider3d(meshFilter, mesh);
+        }
+        //end class
+    }
+}
