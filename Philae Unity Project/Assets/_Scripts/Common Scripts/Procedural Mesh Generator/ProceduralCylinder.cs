38c09b46ac156ce8f806520644f99fddef6772a5
usernameHed <ugo@belfiore.fr>
2020-03-28 21:49:22 +0100

    Add procedural Cylinder

---
+++ Philae Unity Project/Assets/_Scripts/Common Scripts/Procedural Mesh Generator/ProceduralCylinder.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+using Sirenix.OdinInspector;
+using hedCommon.extension.runtime;
+
+namespace hedCommon.procedural
+{
+    /// <summary>
+    /// Plane Description
+    /// </summary>
+    public class ProceduralCylinder : ProceduralCone
+    {
+        /// <summary>
+        /// here generate the mesh...
+        /// </summary>
+        protected override void GenerateMesh()
+        {
+            _topRadius = _radius;
+            base.GenerateMesh();
+        }
+    }
+}
