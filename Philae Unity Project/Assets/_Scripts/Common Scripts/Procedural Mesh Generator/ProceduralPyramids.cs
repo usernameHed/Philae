1944f48698bd8ca462a96a966f8bfb9d7e3b93bc
usernameHed <ugo@belfiore.fr>
2020-03-28 21:50:16 +0100

    Add Procedural Pyramids

---
+++ Philae Unity Project/Assets/_Scripts/Common Scripts/Procedural Mesh Generator/ProceduralPyramids.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+using Sirenix.OdinInspector;
+using hedCommon.extension.runtime;
+
+namespace hedCommon.procedural
+{
+    /// <summary>
+    /// Plane Description
+    /// </summary>
+    public class ProceduralPyramids : ProceduralCone
+    {
+
+    }
+}
