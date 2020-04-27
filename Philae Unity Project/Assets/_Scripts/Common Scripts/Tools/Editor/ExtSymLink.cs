df73c918286a6009c067d930c02cb6371bed3e14
usernameHed <ugo@belfiore.fr>
2020-04-27 19:00:21 +0200

    Add * to gameObject hierarchy if they have stuff in symLinks
    Add * to prefabs's gameObject in hierarchy which are in Symlink Folder, Or to gameObjects with script that are inside Symlink

--- Philae Unity Project/Assets/_Scripts/Common Scripts/Tools/Editor/ExtSymLink.cs
+++ Philae Unity Project/Assets/_Scripts/Common Scripts/Tools/Editor/ExtSymLink.cs
@@ -4,6 +4,8 @@ using System.IO;
 using hedCommon.extension.editor;
 using System.Collections.Generic;
 using hedCommon.extension.runtime;
+using extUnityComponents.transform;
+using extUnityComponents;
 
 namespace hedCommon.tools
 {
@@ -15,13 +17,15 @@ namespace hedCommon.tools
     /// symlinks.
     /// </summary>
     [InitializeOnLoad]
-	public static class ExtSymLink
-	{
-		// FileAttributes that match a junction folder.
-		const FileAttributes FOLDER_SYMLINK_ATTRIBS = FileAttributes.Directory | FileAttributes.ReparsePoint;
+    public static class ExtSymLink
+    {
+        // FileAttributes that match a junction folder.
+        const FileAttributes FOLDER_SYMLINK_ATTRIBS = FileAttributes.Directory | FileAttributes.ReparsePoint;
         const FileAttributes FILE_SYMLINK_ATTRIBS = FileAttributes.Directory & FileAttributes.Archive;
 
         private static List<string> _allSymLinksInProject = new List<string>();
+        private static List<int> _allGameObjectInFrameWork = new List<int>(500);
+        private static List<System.Type> _allNonPersistentType = new List<System.Type>() { typeof(TransformHiddedTools), typeof(AnimatorHiddedTools), typeof(MeshFilterHiddedTools), typeof(RectTransformHiddedTools), typeof(RectTransformHiddedTools) };
 
         // Style used to draw the symlink indicator in the project view.
         private static GUIStyle _symlinkMarkerStyle = null;
@@ -45,6 +49,90 @@ namespace hedCommon.tools
         static ExtSymLink()
 		{
             EditorApplication.projectWindowItemOnGUI += OnProjectWindowItemGUI;
+            EditorApplication.hierarchyWindowItemOnGUI += OnHierarchyItemGUI;
+            EditorApplication.hierarchyChanged += OnHierarchyWindowChanged;
+        }
+
+        [UnityEditor.Callbacks.DidReloadScripts]
+        private static void OnScriptsReloaded()
+        {
+            UpdateListGameObjects();
+        }
+
+        private static void OnHierarchyWindowChanged()
+        {
+            UpdateListGameObjects();
+        }
+
+        private static void UpdateListGameObjects()
+        {
+            // Check here
+            GameObject[] allGameObjects = Object.FindObjectsOfType(typeof(GameObject)) as GameObject[];
+
+            _allGameObjectInFrameWork.Clear();
+            foreach (GameObject g in allGameObjects)
+            {
+                if (IsPrefabsAndInSymLink(g) || HasComponentInSymLink(g))
+                {
+                    _allGameObjectInFrameWork.AddIfNotContain(g.GetInstanceID());
+                }
+            }
+        }
+
+        private static bool IsPrefabsAndInSymLink(GameObject g)
+        {
+            bool isPrefab = ExtPrefabsEditor.IsPrefab(g, out GameObject prefab);
+            if (!isPrefab)
+            {
+                return (false);
+            }
+            Object parentObject = PrefabUtility.GetCorrespondingObjectFromSource(prefab);
+            string path = AssetDatabase.GetAssetPath(parentObject);
+            FileAttributes attribs = File.GetAttributes(path);
+            return (IsAttributeAFileInsideASymLink(path, attribs));
+        }
+
+        /// <summary>
+        /// return true if this gameObject has at least one component inside a symLink
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// <returns></returns>
+        private static bool HasComponentInSymLink(GameObject g)
+        {
+            MonoBehaviour[] monoBehaviours = g.GetComponents<MonoBehaviour>();
+            for (int i = 0; i < monoBehaviours.Length; i++)
+            {
+                MonoBehaviour mono = monoBehaviours[i];
+                if (mono != null && mono.hideFlags == HideFlags.None && !_allNonPersistentType.Contains(mono.GetType()))
+                {
+                    MonoScript script = MonoScript.FromMonoBehaviour(mono); // gets script as an asset
+                    string path = script.GetPath();
+                    FileAttributes attribs = File.GetAttributes(path);
+                    if (IsAttributeAFileInsideASymLink(path, attribs))
+                    {
+                        return (true);
+                    }
+                }
+            }
+            return (false);
+        }
+
+        private static void OnHierarchyItemGUI(int instanceID, Rect selectionRect)
+        {
+            try
+            {
+                // place the icoon to the right of the list:
+                Rect r = new Rect(selectionRect);
+                r.x = r.width - 20;
+                r.width = 18;
+
+                if (_allGameObjectInFrameWork.Contains(instanceID))
+                {
+                    // Draw the texture if it's a light (e.g.)
+                    GUI.Label(r, "*", symlinkMarkerStyle);
+                }
+            }
+            catch { }
         }
 
         /// <summary>
